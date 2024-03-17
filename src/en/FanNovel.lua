49585d49b4fafd74c2a6d81afd56c07bbfb4f949
Adrianjack <139681740+Adrianjack@users.noreply.github.com>
2024-03-17 11:29:55 +0530

    Update FanNovel.lua

--- src/en/FanNovel.lua
+++ src/en/FanNovel.lua
@@ -61,7 +61,7 @@ local GENRES = {
 return Require("FanNovel")("https://www.fannovels.com", {
 	id = 1308639964,
 	name = "FanNovel",
-	imageURL = "https://raw.githubusercontent.com/Adrianjack/Shosetsu-extensions/main/Icons/Untitled56%20(1).png",
+	imageURL = "https://raw.githubusercontent.com/Adrianjack/Shosetsu-extensions/main/Icons/Picsart_24-03-17_11-26-04-475.png",
 	shrinkURLNovel = "^.-fannovels%.com",
 	hasCloudFlare = true,
 

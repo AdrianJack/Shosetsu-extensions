e718eb44fcc3503a5991f94bd7398e16917eaead
Adrianjack <139681740+Adrianjack@users.noreply.github.com>
2024-03-24 19:44:51 +0530

    Update FanNovel-Chapters.lua

--- src/en/FanNovel-Chapters.lua
+++ src/en/FanNovel-Chapters.lua
@@ -1,4 +1,4 @@
--- {"id":1308639968,"ver":"1.1.4","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
+-- {"id":1308639965,"ver":"1.1.4","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
 
 local GENRES = {
 	"All",
@@ -59,7 +59,7 @@ local GENRES = {
 }
 
 return Require("FanNovel-Chapters")("https://www.fannovels.com", {
-	id = 1308639968,
+	id = 1308639965,
 	name = "FanNovel-Chapters",
 	shrinkURLNovel = "^.-fannovels%.com",
 	hasCloudFlare = true,
